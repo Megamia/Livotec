45c62820a776299d5cfc9381953fdfab465c6ebd
Megamia <luudat214@gmail.com>
2025-03-04 08:54:23 +0700

    update

--- FE/src/views/HomePage.vue
+++ FE/src/views/HomePage.vue
@@ -1,35 +1,17 @@
 <template>
-<<<<<<< HEAD
-  <a-flex class="flex-1 flex-col">
-    <DefaultLayout>
-      <HomeHeroSlideComponent />
-      <NavProductComponent />
-      <!-- <ProductComponent :categorySlug="'may-loc-nuoc'" /> -->
-      <ProductComponent :categorySlug="'bep-tu'" />
-    </DefaultLayout>
-  </a-flex>
-=======
+  <a-flex class="flex-1">
     <HomeHeroSlideComponent />
     <NavProductComponent />
     <ProductComponent :categorySlug="'may-loc-nuoc'" />
     <ProductComponent :categorySlug="'bep-tu'" />
->>>>>>> cdc64764f6c81b29d0ca71feff9c76119d5ead85
+  </a-flex>
 </template>
 
 <script setup>
 import HomeHeroSlideComponent from "@/components/HomeHeroSlideComponent.vue";
 import NavProductComponent from "@/components/NavProductComponent.vue";
 import ProductComponent from "@/components/ProductComponent.vue";
-// import { onMounted } from "vue";
-// import store from "@/store/store";
 
-// const fetchData=async()=>{
-//   const dataProduct = store.getters["product/getDataStoreProduct"];
-//   console.log();
-  
-// }
-
-// onMounted(()=>fetchData());
 </script>
 
 <style scoped></style>
