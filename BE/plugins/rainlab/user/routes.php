8c382d675f76b2e7a8b197f0e0738bb840785b6b
BetoAnh <vtahvp@gmail.com>
2025-03-07 10:14:31 +0700

    update reset password

--- BE/plugins/rainlab/user/routes.php
+++ BE/plugins/rainlab/user/routes.php
@@ -1,6 +1,7 @@
 <?php
 use Illuminate\Http\Request;
 use RainLab\User\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 Route::group(['prefix' => 'apiUser'], function () {
     Route::post('profile', function (Request $request) {
@@ -31,17 +32,33 @@ Route::group(['prefix' => 'apiUser'], function () {
         }
         $validated = $request->validate([
             'old_password' => 'required',
-            'new_password' => 'required',
+            'new_password' => [
+                'required',
+                'string',
+                'min:8',  // Ít nhất 8 ký tự
+                'regex:/[A-Z]/', // Ít nhất một chữ hoa
+                'regex:/[a-z]/', // Ít nhất một chữ thường
+                'regex:/[0-9]/', // Ít nhất một số
+                function ($attribute, $value, $fail) use ($request, $user) {
+                    if (Hash::check($value, $user->password)) {
+                        $fail('Mật khẩu mới không được trùng với mật khẩu cũ.');
+                    }
+                },
+            ],
         ]);
         // Kiểm tra mật khẩu cũ
         if (!Hash::check($validated['old_password'], $user->password)) {
             return response()->json(['error' => 'Mật khẩu cũ không chính xác!'], 422);
         }
         // Cập nhật mật khẩu mới
-        $user->password = Hash::make($validated['new_password']);
+        // $user->password = Hash::make($validated['new_password']);
+        // $user->save();
+        // // Ghi log sự kiện đổi mật khẩu
+        // \Log::info("User ID {$user->id} đã đổi mật khẩu.", ['user_id' => $user->id]);
+        \Log::info('Hashed Password Before Save: ' . Hash::make($validated['new_password']));
+        $user->password = $validated['new_password'];
         $user->save();
-        // Ghi log sự kiện đổi mật khẩu
-        \Log::info("User ID {$user->id} đã đổi mật khẩu.", ['user_id' => $user->id]);
+        \Log::info('Hashed Password After Save: ' . $user->password);
         return response()->json([
             'message' => 'Đổi mật khẩu thành công!',
             'user' => [
